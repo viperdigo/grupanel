22d89c8def2ff807bf3760d5859ddaeecbf1b61d
Rodrigo Alfieri <viperdigo@gmail.com>
2018-11-27 17:58:19 -0200

    Update Deploy DEV

--- app/config/deploy/dev.rb
+++ app/config/deploy/dev.rb
@@ -3,5 +3,7 @@ role :web, "grupanel-dev.com"
 
 set :deploy_to, "/var/www/html/#{application}/dev"
 set :user, "root"
-set :symfony_env,  "dev"
+set :symfony_env_prod, "dev"
+set :clear_controllers, false
+set :controllers_to_clear, ['']
 set :keep_releases,   3
